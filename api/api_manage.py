7271e0a347b1e5d79d95411c733365f2da58c2c4
dw <duwan0525@163.com>
2021-03-19 22:09:53 +0800

    manage api

---
+++ api/api_manage.py
@@ -0,0 +1,19 @@
+class ApiManage(object):
+    def __init__(self):
+        pass
+
+    # 扣款
+    def api_capture(self):
+        pass
+
+    # 取消
+    def api_cancell(self):
+        pass
+
+    # 退款
+    def api_refund(self):
+        pass
+
+    # 拒付
+    def api_charge(self):
+        pass
